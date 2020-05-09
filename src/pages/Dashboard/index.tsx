7235af8f56be29ea521425a7fc1ae11d1862b9ea
Leozartino <leozartino@gmail.com>
2020-05-09 18:05:07 -0300

    feat(src): add styles at Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,54 @@
 import React from 'react';
-import { Title } from './style';
+import { FiChevronRight } from 'react-icons/fi';
+import logo from '../../assets/github-arrow.svg';
+import { Title, Form, ListRepositories } from './style';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logo} alt="github-arrow" />
+      <Title>Explore repositórios no Github</Title>
+      <Form action="">
+        <input type="text" placeholder="Informe o repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+      <ListRepositories>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/38565099?s=460&u=5e68e27f8b2b3506b9468720d570cf0265b4207f&v=4"
+            alt=""
+          />
+          <div id="container-info-repository">
+            <strong>leozartino/js</strong>
+            <p>Descrição do repositório</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/38565099?s=460&u=5e68e27f8b2b3506b9468720d570cf0265b4207f&v=4"
+            alt=""
+          />
+          <div id="container-info-repository">
+            <strong>leozartino/js</strong>
+            <p>Descrição do repositório</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/38565099?s=460&u=5e68e27f8b2b3506b9468720d570cf0265b4207f&v=4"
+            alt=""
+          />
+          <div id="container-info-repository">
+            <strong>leozartino/js</strong>
+            <p>Descrição do repositório</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </ListRepositories>
+    </>
+  );
 };
 
 export default Dashboard;
--- src/pages/Dashboard/style.ts
+++ src/pages/Dashboard/style.ts
@@ -1,6 +1,92 @@
 import styled from 'styled-components';
+import { shade } from 'polished';
 
 export const Title = styled.h1`
-  font-size: 48px;
+  margin-top: 70px;
+  font-size: 40px;
+  max-width: 450px;
+  line-height: 50px;
   color: #3a3a3a;
+  text-shadow: 2px 7px 5px rgba(0, 0, 0, 0.3),
+    0px -4px 10px rgba(255, 255, 255, 0.3);
+`;
+
+export const Form = styled.form`
+  margin-top: 40px;
+  max-width: 700px;
+  display: flex;
+
+  input {
+    flex: 1;
+    height: 70px;
+    padding: 0 24px;
+    border-radius: 5px 0 0 5px;
+    color: #3a3a3a;
+
+    &::placeholder {
+      color: #a8a8b3;
+    }
+  }
+
+  button {
+    width: 200px;
+    height: 70px;
+    background: #76bfd8;
+    border-radius: 0 5px 5px 0;
+    border: 0;
+    font-weight: bold;
+    transition: background-color 0.2s;
+    &:hover {
+      background: ${shade(0.2, '#76bfd8')};
+    }
+  }
+`;
+
+export const ListRepositories = styled.div`
+  margin-top: 80px;
+  max-width: 700px;
+
+  a {
+    background: #fff;
+    border-radius: 5px;
+    width: 100%;
+    padding: 18px;
+    display: block;
+    text-decoration: none;
+    display: flex;
+    align-items: center;
+    font-size: 5px;
+    transition: transform 0.2s;
+
+    & + a {
+      margin-top: 16px;
+    }
+
+    &:hover {
+      transform: translateX(10px);
+    }
+
+    img {
+      width: 64px;
+      height: 64px;
+      border-radius: 50%;
+    }
+    #container-info-repository {
+      margin-left: 16px;
+      strong {
+        font-size: 18px;
+        color: #3d3d4d;
+      }
+
+      p {
+        font-size: 15px;
+        color: #a8a8b3;
+        margin-top: 4px;
+      }
+    }
+    svg {
+      margin-left: auto;
+      color: #cbcbd6;
+    }
+  }
 `;
