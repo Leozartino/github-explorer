7b6933255395d79b8024b968c22acda0171bad6d
Leozartino <leozartino@gmail.com>
2020-05-11 21:53:44 -0300

    feat: add error handling

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,51 +1,67 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 import logo from '../../assets/github-arrow.svg';
-import { Title, Form, ListRepositories } from './style';
+import { Title, Form, ListRepositories, Error } from './style';
+import api from '../../services/apiClient';
+
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
 
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [inputError, setInputError] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+
+    if (!newRepo) {
+      setInputError('Informe: autor/repositorio');
+      return;
+    }
+    try {
+      const response = await api.get<Repository>(`repos/${newRepo}`);
+
+      setRepositories([...repositories, response.data]);
+      setNewRepo('');
+      setInputError('');
+    } catch (error) {
+      setInputError('Não foi possível encontrar esse repositório.');
+    }
+  }
   return (
     <>
       <img src={logo} alt="github-arrow" />
       <Title>Explore repositórios no Github</Title>
-      <Form action="">
-        <input type="text" placeholder="Informe o repositório" />
+      <Form hasError={!!inputError} onSubmit={handleAddRepository}>
+        <input
+          type="text"
+          placeholder="Informe o repositório"
+          onChange={(e) => setNewRepo(e.target.value)}
+          value={newRepo}
+        />
         <button type="submit">Pesquisar</button>
       </Form>
+      {inputError && <Error>{inputError}</Error>}
       <ListRepositories>
-        <a href="teste">
-          <img
-            src="https://avatars0.githubusercontent.com/u/38565099?s=460&u=5e68e27f8b2b3506b9468720d570cf0265b4207f&v=4"
-            alt=""
-          />
-          <div id="container-info-repository">
-            <strong>leozartino/js</strong>
-            <p>Descrição do repositório</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img
-            src="https://avatars0.githubusercontent.com/u/38565099?s=460&u=5e68e27f8b2b3506b9468720d570cf0265b4207f&v=4"
-            alt=""
-          />
-          <div id="container-info-repository">
-            <strong>leozartino/js</strong>
-            <p>Descrição do repositório</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img
-            src="https://avatars0.githubusercontent.com/u/38565099?s=460&u=5e68e27f8b2b3506b9468720d570cf0265b4207f&v=4"
-            alt=""
-          />
-          <div id="container-info-repository">
-            <strong>leozartino/js</strong>
-            <p>Descrição do repositório</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
+        {repositories.map((repository) => (
+          <a key={repository.full_name} href="infos">
+            <img src={repository.owner.avatar_url} alt="avatar" />
+            <div id="container-info-repository">
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </ListRepositories>
     </>
   );
--- src/pages/Dashboard/style.ts
+++ src/pages/Dashboard/style.ts
@@ -1,5 +1,9 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { shade } from 'polished';
+
+interface FormProps {
+  hasError: boolean;
+}
 
 export const Title = styled.h1`
   margin-top: 70px;
@@ -11,7 +15,7 @@ export const Title = styled.h1`
     0px -4px 10px rgba(255, 255, 255, 0.3);
 `;
 
-export const Form = styled.form`
+export const Form = styled.form<FormProps>`
   margin-top: 40px;
   max-width: 700px;
   display: flex;
@@ -22,6 +26,14 @@ export const Form = styled.form`
     padding: 0 24px;
     border-radius: 5px 0 0 5px;
     color: #3a3a3a;
+    border: 2px solid #fff;
+
+    ${(props) =>
+      props.hasError &&
+      css`
+        border-right: 0;
+        border-color: #c53030;
+      `}
 
     &::placeholder {
       color: #a8a8b3;
@@ -72,7 +84,8 @@ export const ListRepositories = styled.div`
       border-radius: 50%;
     }
     #container-info-repository {
-      margin-left: 16px;
+      margin: 0 16px;
+      flex: 1;
       strong {
         font-size: 18px;
         color: #3d3d4d;
@@ -89,4 +102,10 @@ export const ListRepositories = styled.div`
       color: #cbcbd6;
     }
   }
+`;
+
+export const Error = styled.span`
+  display: block;
+  color: #c53030;
+  margin-top: 8px;
 `;
