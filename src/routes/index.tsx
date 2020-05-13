2952a7239e3d1b81c0f15dda35bc9373c6e41cac
Leozartino <leozartino@gmail.com>
2020-05-13 15:55:58 -0300

    refactor: change name of Dahsboard componnet

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import Dashboad from '../pages/Dashboard';
+import Dashborad from '../pages/Dashboard';
 import Repository from '../pages/Repository';
 
 const Routes: React.FC = () => (
   <Switch>
-    <Route path="/" exact component={Dashboad} />
-    <Route path="/repository" component={Repository} />
+    <Route path="/" exact component={Dashborad} />
+    <Route path="/repository/:nameRepository+" component={Repository} />
   </Switch>
 );
 
