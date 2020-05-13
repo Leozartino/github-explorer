b422fa26a332be10e54803ee5bbf5775de862fa8
Leozartino <leozartino@gmail.com>
2020-05-13 15:54:28 -0300

    refactor:  name of route repository

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,9 +1,45 @@
 import React from 'react';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft } from 'react-icons/fi';
+import { Header, RepositoryInfo, Issue } from './style';
+import logo from '../../assets/github-arrow.svg';
 
-
-const Repository:React.FC = () => {
-  return <h1>Repository</h1>;
+interface RepositoryParams {
+  nameRepository: string;
 }
 
+const Repository: React.FC = () => {
+  const { params } = useRouteMatch<RepositoryParams>();
+  return (
+    <>
+      <Header>
+        <img src={logo} alt="GitHub Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} /> Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img src="" alt="img" />
+          <div>
+            <strong />
+            <p />
+          </div>
+        </header>
+        <ul />
+      </RepositoryInfo>
+
+      <Issue>
+        <Link to="/">
+          <div>
+            <strong />
+            <p />
+          </div>
+        </Link>
+      </Issue>
+    </>
+  );
+};
 
 export default Repository;
